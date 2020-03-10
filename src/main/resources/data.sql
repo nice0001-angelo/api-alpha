09ae9671f392c39acf3b1bb388efb8ff4814b77c
nice0001-angelo <nice0001@gmail.com>
2020-03-10 10:49:13 -0400

    update initial data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
-insert into beer(name, abv) values ('Nam', 7.7);
-insert into beer(name, abv) values ('Jee', 9.2);
-insert into beer(name, abv) values ('Angelo', 8.8);
+insert into beer(name, abv) values ('Cas', 7.7);
+insert into beer(name, abv) values ('Corona', 9.2);
+insert into beer(name, abv) values ('Millar', 8.8);
+insert into beer(name, abv) values ('Max', 8.8);
 
 commit;
