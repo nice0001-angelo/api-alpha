3974419b686aedba5cd17e093c1af659f32750d4
nice0001-angelo <nice0001@gmail.com>
2020-03-10 07:53:38 -0400

    update model

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into beers(name, abv) values ('Nam', 7.7);
-insert into beers(name, abv) values ('Jee', 9.2);
-insert into beers(name, abv) values ('Angelo', 8.8);
+insert into beer(name, abv) values ('Nam', 7.7);
+insert into beer(name, abv) values ('Jee', 9.2);
+insert into beer(name, abv) values ('Angelo', 8.8);
 
 commit;
