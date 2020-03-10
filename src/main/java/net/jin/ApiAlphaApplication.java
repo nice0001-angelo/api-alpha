8de871e3279bd24689749c7bc17a7efb98ed0200
nice0001-angelo <nice0001@gmail.com>
2020-03-10 05:01:37 -0400

    initial api-alpha

---
+++ src/main/java/net/jin/ApiAlphaApplication.java
@@ -0,0 +1,13 @@
+package net.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAlphaApplication.class, args);
+	}
+
+}
