c52d9cf9416db38176697ed7ddb51a4af3d84034
nice0001-angelo <nice0001@gmail.com>
2020-03-10 05:39:25 -0400

    update persistance

--- src/main/java/net/jin/Beer.java
+++ src/main/java/net/jin/Beer.java
@@ -2,13 +2,14 @@ package net.jin;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Beer {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String name;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into beers(name, abv) values ('Nam', 7.7);
+insert into beers(name, abv) values ('Jee', 9.2);
+insert into beers(name, abv) values ('Angelo', 8.8);
+
+commit;
