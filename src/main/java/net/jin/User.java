ccc07d0a5bf06c523d5a780b75b118c400570283
nice0001-angelo <nice0001@gmail.com>
2020-03-11 02:50:34 -0400

    add User API

--- src/main/java/net/jin/User.java
+++ src/main/java/net/jin/User.java
@@ -16,7 +16,37 @@ public class User {
 	private String sex;
 	
 	private Long height;
-	
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public Long getHeight() {
+		return height;
+	}
+
+	public void setHeight(Long height) {
+		this.height = height;
+	}
 
 }
